fba97a4bbd427a29ba68a7b0b3566d7b71103f55
deleiscorrea <deleiscorrea13@gmail.com>
2024-12-20 18:25:36 -0300

    back

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express()
 const PORT = ENVIROMENT.PORT || 3000
 
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://frontend-q-pasa-d908zpg13-ignacios-projects-ff75f73a.vercel.app"],
+    origin: ["http://localhost:5173", "https://frontend-q-pasa.vercel.app"],
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
 
