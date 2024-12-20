72897b89d1edcb40066c275127eb21c065c39532
deleiscorrea <deleiscorrea13@gmail.com>
2024-12-20 18:44:43 -0300

    back

--- src/server.js
+++ src/server.js
@@ -10,12 +10,7 @@ import contactRouter from "./routes/contacts.router.js"
 const app = express()
 const PORT = ENVIROMENT.PORT || 3000
 
-const corsOptions = {
-    origin: ["http://localhost:5173", "https://frontend-q-pasa.vercel.app"],
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
-
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json({limit: '5mb'}))
 app.use(verifyApyKeyMiddleware)
 
