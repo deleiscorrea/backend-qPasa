9b7fe1d26f675279f29c285de9904d794eff01bb
deleiscorrea <deleiscorrea13@gmail.com>
2024-12-22 13:50:25 -0300

    cors

--- src/db/config.js
+++ src/db/config.js
@@ -1,16 +1,17 @@
 import mongoose from "mongoose"
 import ENVIROMENT from "../config/enviroment.config.js"
 
-mongoose.connect(ENVIROMENT.DB_URL)
-.then(
-    () => {
-        console.log('Conectado a la base de datos')
-    }
-)
-.catch(
-    (error) => {
-        console.error("Error al conectar a la base de datos")
-    }
-)
+mongoose.connect(ENVIROMENT.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,  // 5 segundos de espera antes de agotar el tiempo
+    connectTimeoutMS: 10000,         // 10 segundos para establecer la conexión
+})
+.then(() => {
+    console.log('Conectado a la base de datos');
+})
+.catch((error) => {
+    console.error("Error al conectar a la base de datos:", error);
+});
 
 export default mongoose
