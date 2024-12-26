cdab11c6865084941709d248e171f19d88b2fb75
deleiscorrea <deleiscorrea13@gmail.com>
2024-12-26 17:31:58 -0300

    auth

--- src/utils/mail.util.js
+++ src/utils/mail.util.js
@@ -3,7 +3,7 @@ import transporter from "../config/transporter.config.js"
 const sendEmail = async (options) => {
     try{
         let response = await transporter.sendMail(options)
-        //console.log(response)
+        console.log(response)
     }
     catch(error){    
         console.error("error al enviar mail:", error)
